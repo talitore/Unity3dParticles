d76852cc2cdc3b63d5001831fc6a4e51981205cb
Taylor E Kems <tekems@gmail.com>
2015-01-19 14:10:43 -0600

    Initial commit

---
+++ Assets/FollowThis.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowThis : MonoBehaviour {
+    public GameObject followThis;
+    public float XOffSet;
+    public float ZOffSet;
+    public float YOffSet;
+	// Use this for initialization
+    void Start() {
+        transform.LookAt(followThis.transform);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 pos = followThis.transform.position;
+        pos.x += XOffSet;
+        pos.z += ZOffSet;
+        pos.y += YOffSet;
+        transform.position = pos;
+    }
+}
---
+++ Assets/WeaponizeExport/BasicShots/BasicShot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicShot : MonoBehaviour {
+
+    public float SpinSpeed = 5f;
+    public float MoveSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.forward * (Time.deltaTime * SpinSpeed), Space.World);
+        transform.Translate(transform.forward * (Time.deltaTime * MoveSpeed));
+	}
+}
